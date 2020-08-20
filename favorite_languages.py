77a2c764ff1e0ce32347daeabb51ee17bb6b83a7
[Pedro Baldera] <[valid-email]>
2020-08-20 17:42:38 -0400

    first comit

--- favorite_languages.py
+++ favorite_languages.py
@@ -6,6 +6,9 @@ favorite_languages = {
 }
 
 for name, languages in favorite_languages.items():
-    print(f"{name.title()}'s favorite languages are:")
+    if len(languages) > 1:
+        print(f"{name.title()}'s favorite languages are:")
+    else:
+        print(f"{name.title()}'s favorite language is:")
     for language in languages:
         print(f"\t{language.title()}")
