9ad817e9e685529dcce6fbe756ea29a8a3f40ff9
[Pedro Baldera] <[valid-email]>
2020-08-23 11:09:13 -0400

    first comit

---
+++ message.py
@@ -0,0 +1,8 @@
+message = input("Tell me what you are learning about the chapter: ")
+
+def display_message(message):
+    """Display sentence provided by user"""
+    print(f"{message.title()}")
+
+display_message(message)
+
