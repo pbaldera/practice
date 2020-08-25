75aacba6c5d6efd86ccea6a76a40c8ba9d022733
[Pedro Baldera] <[valid-email]>
2020-08-25 19:57:23 -0400

    first comit

--- electric_car.py
+++ electric_car.py
@@ -32,6 +32,18 @@ class Car:
         self.odometer_reading += miles
 
 
+class Battery:
+    """A simple attempt to model a battery for an electric car."""
+
+    def __init__(self, battery_size=75):
+        """Initialize the battery's attributes"""
+        self.battery_size = battery_size
+
+    def describe_battery(self):
+        """Print a statement describing the battery size."""
+        print(f"This car has a {self.battery_size}-KWh battery.")
+
+
 class ElectricCar(Car):
     """Represent aspects of a car, specific to electric vehicles."""
 
@@ -41,13 +53,9 @@ class ElectricCar(Car):
         Then initialize attributes specific to an electric car.
         """
         super().__init__(make, model, year)
-        self.battery_size = 75
-
-    def describe_battery(self):
-        """Print a statement describing the battery size."""
-        print(f"This car has a {self.battery_size}-KWh battery.")
+        self.battery = Battery()
 
 
 my_tesla = ElectricCar('tesla', 'model s', 2019)
 print(my_tesla.get_descriptive_name())
-my_tesla.describe_battery()
+my_tesla.battery.describe_battery()
