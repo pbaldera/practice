a207f295b0696c1d6254ac700a9871d469785f17
[Pedro Baldera] <[valid-email]>
2020-08-26 06:43:40 -0400

    first comit

--- car.py
+++ car.py
@@ -1,3 +1,6 @@
+"""A class that can be used to represent a car."""
+
+
 class Car:
     """A simple attempt to represent a car."""
 
@@ -32,17 +35,34 @@ class Car:
         self.odometer_reading += miles
 
 
-my_used_car = Car('subaru', 'outback', 2015)
-print(my_used_car.get_descriptive_name())
+class Battery:
+    """A simple attempt to model a battery for an electric car."""
 
-my_used_car.update_odometer(23_500)
-my_used_car.read_odometer()
+    def __init__(self, battery_size=75):
+        """Initialize the battery's attributes"""
+        self.battery_size = battery_size
 
-my_used_car.increment_odometer(100)
-my_used_car.read_odometer()
+    def describe_battery(self):
+        """Print a statement describing the battery size."""
+        print(f"This car has a {self.battery_size}-KWh battery.")
 
-my_new_car = Car('audi', 'a4', 2019)
-print(my_new_car.get_descriptive_name())
+    def get_range(self):
+        """Print a statement about the range this battery provides"""
+        if self.battery_size == 75:
+            range = 260
+        elif self.battery_size == 100:
+            range = 315
 
-my_new_car.odometer_reading = 23
-my_new_car.read_odometer()
+        print(f"This car can go about {range} miles on a full charge.")
+
+
+class ElectricCar(Car):
+    """Represent aspects of a car, specific to electric vehicles."""
+
+    def __init__(self, make, model, year):
+        """
+        Initialize attributes of the parent class.
+        Then initialize attributes specific to an electric car.
+        """
+        super().__init__(make, model, year)
+        self.battery = Battery()
--- electric_car.py
+++ electric_car.py
@@ -43,6 +43,15 @@ class Battery:
         """Print a statement describing the battery size."""
         print(f"This car has a {self.battery_size}-KWh battery.")
 
+    def get_range(self):
+        """Print a statement about the range this battery provides"""
+        if self.battery_size == 75:
+            range = 260
+        elif self.battery_size == 100:
+            range = 315
+
+        print(f"This car can go about {range} miles on a full charge.")
+
 
 class ElectricCar(Car):
     """Represent aspects of a car, specific to electric vehicles."""
@@ -59,3 +68,4 @@ class ElectricCar(Car):
 my_tesla = ElectricCar('tesla', 'model s', 2019)
 print(my_tesla.get_descriptive_name())
 my_tesla.battery.describe_battery()
+my_tesla.battery.get_range()
